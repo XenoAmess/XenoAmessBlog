92b9970282f9a48d25f9e2b71333aafb4a05efa0
XenoAmess <xenoamess@gmail.com>
2021-03-13 01:41:06 +0800

    add update shell

--- update.sh
+++ update.sh
@@ -1,1 +1,1 @@
-cd /hexo/source/XenoAmessBlog ; git pull --force ; cp -rf ./_posts ../ ; cp -rf ./resources ../
+cd /hexo/source/XenoAmessBlog ; git pull --force ; cp -rf ./_posts ../ ; cp -rf ./resources ../ ; cp -f ./favicon.png ../
